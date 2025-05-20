df99eb0809d1ab000deaed041be7f90f90122712
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-05-20 06:14:52 +0600

    Implement createToken and createMarketItem functions in Cryptau contract

--- contracts/Cryptau.sol
+++ contracts/Cryptau.sol
@@ -47,4 +47,36 @@ contract Cryptau is ERC721URIStorage{
    function getListingPrice() public view returns (uint256){
       return listingPrice;
    }
+
+   function createToken(string memory tokenURI,uint256 price) public payable returns (uint){
+      _tokenIds.increment();
+
+      uint256 newTokenId = _tokenIds.current();
+      _mint(msg.sender, newTokenId);
+      _setTokenURI(newTokenId, tokenURI);
+
+      createMarketItem(newTokenId,price);
+
+      return newTokenId;
+
+   }
+
+   function createMarketItem(uint256 tokenId, uint256 price) private {
+      require(price > 0, "Price must be atleast 1" );
+      require(msg.value == listingPrice, "Price must be equal to listing price");
+
+      idToMarketItem[tokenId] = MarketItem(
+         tokenId,
+         payable(msg.sender),
+         payable(address(this)),
+         price,
+         false
+      );
+
+      _transfer(msg.sender, address(this), tokenId);
+
+      emit MarketItemCreated(tokenId, msg.sender, address(this), price, false);
+   }
+
+   
 }
