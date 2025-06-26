59663a9878bb7025023f7535652a59fc80765df0
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-06-26 23:00:16 +0600

    feat: add Loader, Modal, and SearchBar components with respective functionalities

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -6,12 +6,12 @@ import images from '../assets';
 import { NFTContext } from '../context/NFTContext';
 import { shortenAddress } from '../utils/shortenAddress';
 
-const NFTCard = ({ nft }) => {
+const NFTCard = ({ nft, onProfilePage }) => {
   const { nftCurrency } = useContext(NFTContext);
   return (
     <Link href={{ pathname: '/nft-details', query: nft }}>
       <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-light-purple rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md">
-        <div className="relative w-full h-52 sm:h-36 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden"><Image
+        <div className="relative w-full h-52 sm:h-36 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden"><Image
           src={nft.image || images[`nft${nft.i}`]}
           layout="fill"
           objectFit="cover"
@@ -22,7 +22,7 @@ const NFTCard = ({ nft }) => {
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{nft.name}</p>
           <div className="flexBetween mt-1 minlg:mt-3 flex-row xs:flex-col xs:items-start xs:mt-3">
             <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg">{nft.price} <span className="normal">{nftCurrency}</span></p>
-            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{nft.seller.length > 10 ? shortenAddress(nft.seller) : nft.seller }</p>
+            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{ shortenAddress(onProfilePage ? nft.owner : nft.seller) }</p>
           </div>
         </div>
       </div>
--- components/index.js
+++ components/index.js
@@ -4,5 +4,9 @@ import Button from './Button';
 import Banner from './Banner';
 import CreatorCard from './CreatorCard';
 import Input from './Input';
+import Loader from './Loader';
+import NFTCard from './NFTCard';
+import Modal from './Modal';
+import SearchBar from './SearchBar';
 
-export { Navbar, Footer, Button, Banner, CreatorCard, Input };
+export { Navbar, Footer, Button, Banner, CreatorCard, Input, Loader, NFTCard, Modal, SearchBar };
