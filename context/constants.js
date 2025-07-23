dcd9784afb9c27a4ac2089102a89a95abe6577b4
Tauhidur Rahman Tauhid <127976351+tautau08@users.noreply.github.com>
2025-07-23 08:11:09 +0600

    Update MarketAddress constant
    Changed the MarketAddress value in constants.js to reflect a new contract address. This ensures the application interacts with the correct smart contract.

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,5 @@
 import marketABI from './Cryptau.json';
 
-export const MarketAddress = '0x0bca6353f80bBe401233d5Ed7732cEACEb13bc88';
+export const MarketAddress = '0x1f5E9E9602bEb4D14c38952cB5504E4471E3328F';
 export const RPC_URL = 'https://carrot.megaeth.com/rpc';
 export const MarketAddressABI = marketABI.abi;
