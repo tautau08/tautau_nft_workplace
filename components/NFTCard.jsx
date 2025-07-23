5af774ffde4b1cc4e8cb461bf8e97d80e7f4fd70
Tauhidur Rahman Tauhid <127976351+tautau08@users.noreply.github.com>
2025-07-23 06:47:42 +0600

    Update NFTCard layout to use flex-col
    Changed the layout of the price and address section in NFTCard from flex-row to flex-col for improved vertical stacking and consistency across screen sizes.

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -20,7 +20,7 @@ const NFTCard = ({ nft, onProfilePage }) => {
         </div>
         <div className="mt-3 flex flex-col">
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{nft.name}</p>
-          <div className="flexBetween mt-1 minlg:mt-3 flex-row xs:flex-col xs:items-start xs:mt-3">
+          <div className="flexBetween mt-1 minlg:mt-3 flex-col xs:flex-col xs:items-start xs:mt-3">
             <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg">{nft.price} <span className="normal">{nftCurrency}</span></p>
             <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{ shortenAddress(onProfilePage ? nft.owner : nft.seller) }</p>
           </div>
